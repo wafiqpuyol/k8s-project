876ad75fa60dcbb05b4962ec44505bc47563369c
wafiqpuyol <wafiqgithub775@gmail.com>
2026-07-27 23:58:58 +0600

    (proj-10): update backend code

--- project 10/app-code/backend/server.ts
+++ project 10/app-code/backend/server.ts
@@ -1,4 +1,7 @@
 import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
 
@@ -6,6 +9,6 @@ app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000')
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(`Server is running on http://localhost:${process.env.SERVER_PORT}`)
 })
